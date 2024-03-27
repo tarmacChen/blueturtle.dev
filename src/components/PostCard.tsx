c023dacc5461751aaf47f0e14e8cdca7ddd56921
tarmac.chen@gmail.com <tarmac.chen@gmail.com>
2024-03-27 16:35:57 +0800

    replace a to next/Link

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,22 +5,23 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Badge } from "@/components/ui/badge"
-import { MarkdownFile } from "mdman"
-import moment from "moment"
+} from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+import { MarkdownFile } from 'mdman';
+import moment from 'moment';
+import Link from 'next/link';
 
 export const PostCard = ({ mdFile }: { mdFile: MarkdownFile }) => {
-  const meta = mdFile.metadata
-  const linkUrl = `/posts/${meta?.title}`
-  const createdTime = moment(meta.createdTime)
-  const tags = meta.tags ? meta.tags.join(", ") : ""
+  const meta = mdFile.metadata;
+  const linkUrl = `/posts/${meta?.title}`;
+  const createdTime = moment(meta.createdTime);
+  const tags = meta.tags ? meta.tags.join(', ') : '';
 
-  const DraftBadge = () => <Badge className="bg-red-600">Draft</Badge>
-  const CategoryBadge = () => <Badge>{meta.category}</Badge>
+  const DraftBadge = () => <Badge className="bg-red-600">Draft</Badge>;
+  const CategoryBadge = () => <Badge>{meta.category}</Badge>;
 
   return (
-    <a href={linkUrl}>
+    <Link href={linkUrl}>
       <Card>
         <CardHeader>
           <CardTitle>{meta?.title}</CardTitle>
@@ -31,11 +32,11 @@ export const PostCard = ({ mdFile }: { mdFile: MarkdownFile }) => {
             <div>{meta?.description}</div>
             <div className="flex flex-row justify-between">
               {meta.draft ? <DraftBadge /> : <CategoryBadge />}
-              <div>{createdTime.format("ll")}</div>
+              <div>{createdTime.format('ll')}</div>
             </div>
           </div>
         </CardContent>
       </Card>
-    </a>
-  )
-}
+    </Link>
+  );
+};
