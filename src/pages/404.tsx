2ebc861d4623a327245b01b898a0e3e74e8bb813
tarmac.chen@gmail.com <tarmac.chen@gmail.com>
2024-04-03 15:22:34 +0800

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { BasicPage } from '@/components/BasicPage';
+export default function NotFoundPage() {
+  return (
+    <BasicPage>
+      <div>404 - Not Found</div>
+    </BasicPage>
+  );
+}
