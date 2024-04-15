0ac89b78b294f01f1261931f816bf3e1b49663cd
tarmac.chen@gmail.com <tarmac.chen@gmail.com>
2024-04-15 10:43:14 +0800

    adjust style

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -22,9 +22,9 @@ export default function PostListPage({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   {
     const oddItemClasses =
-      'hover:bg-blue-100 dark:bg-gray-800 dark:hover:bg-gray-800 hover:border-2 border-blue-500';
+      'hover:bg-blue-100 dark:bg-gray-800 dark:hover:bg-gray-800 hover:border-2 border-blue-500 bg-gray-100';
     const evenItemClasses =
-      'hover:bg-blue-100 bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-700 hover:border-2 border-blue-500';
+      'hover:bg-blue-100 bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-700 hover:border-2 border-blue-500';
 
     const PostItems = mdFiles.map((file) => {
       const url = `/posts/${file.metadata?.title}`;
