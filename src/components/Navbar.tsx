8b6b44a428ec29ff80b0da93f988c2a799ee0d61
tarmac.chen@gmail.com <tarmac.chen@gmail.com>
2024-04-17 16:24:23 +0800

    adjust SearchBar max-width on mobile device

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,13 +29,13 @@ export const NavbarWithSearchBar = ({
       <div className="invisible md:visible flex flex-row justify-center mx-auto">
         <DesktopNavbar />
       </div>
-      <div className="flex flex-row gap-2 justify-between items-center">
+      <div className="flex flex-row gap-2 justify-between items-center max-w-44">
         <SearchBar
           search={search}
           dispatch={dispatch}
         />
-        <ToggleThemeButton />
       </div>
+      <ToggleThemeButton />
     </div>
   );
 };
