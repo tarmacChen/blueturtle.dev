5194b6daceab61fd3f1e38d190bf41e79a10161d
tarmac.chen@gmail.com <tarmac.chen@gmail.com>
2024-09-12 07:58:48 +0800

    add -> favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1"></meta>
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
       </Head>
 
       <ThemeProvider
