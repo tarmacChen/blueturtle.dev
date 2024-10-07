7d0999b140995326f61fc31877089a1fee84b50d
tarmac.chen@gmail.com <tarmac.chen@gmail.com>
2024-10-08 07:33:32 +0800

    adjust -> remove useless className

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@/components/ui/button';
-import { mergeClassNames } from '@/lib/helper';
-import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { mergeClassNames } from "@/lib/helper";
+import Link from "next/link";
 
 export function Logo({ className }: { className?: string }) {
   const logoClasses = mergeClassNames(
-    className || '',
-    'bg-blue-600 hover:bg-blue-600 hover:shadow-md hover:font-sem active:bg-primary active:shadow-none active:font-medium text-white active:bg-blue-500',
+    className || "",
+    "bg-blue-600 hover:bg-blue-600 hover:shadow-md active:bg-primary active:shadow-none active:font-medium text-white active:bg-blue-500",
   );
   return (
     <Button
